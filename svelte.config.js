ef26311671fc2c5d54326f907e161ef370835605
sidney-prytherch <sidneyprytherch@gmail.com>
2025-11-27 14:16:27 -0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
             base: dev ? '' : '/blue-poke'
         },
         alias: {
-            $img: dev ? "assets" : "/blue-poke/assets"
+            $img: 'blue-poke/assets'
         }
     },
     preprocess: vitePreprocess(),
